5f2a0d86482ec994f94b98b35748b09de9684dbb
skycow <skyler.cowley@gmail.com>
2015-03-26 19:06:38 -0600

    Working2&3

--- Problem2/Problem2/Problem2.cpp
+++ Problem2/Problem2/Problem2.cpp
@@ -2,7 +2,7 @@
 # include <iomanip>
 
 using namespace std;
-const int nn = 5, mm = 3;
+const int nn = 5, mm = 4;
 
 void mtranspose(double M[nn][mm], double MT[mm][nn], int n, int m)
 {//Calculates transpose of matrix M(nxm) as MT(mxn)
@@ -104,8 +104,8 @@ void Decompose(double A[mm][mm], int n, double tol, int O[], double s[], int er)
 	};
 	if (abs(A[O[k]][k] / s[O[k]]) < tol)
 	{
-		//er = -1; cout << "trouble2" << endl;
-		er = -1; cout << endl;
+		er = -1; cout << "trouble2" << endl;
+		//er = -1; cout << endl;
 	}
 };
 
@@ -181,22 +181,22 @@ void NLRegress(double Z[nn][mm], double y[], double a[], int n, int m){
 	
 }
 
-/*void BuildZP(double Z[nn][mm], double x[],int n,int m){
+void BuildZP(double Z[nn][mm], double x[],int n,int m){
 	for(int i=0; i<n; i++){
 		for(int j=0; j<m; j++){
 			Z[i][j] = pow(x[i],j);
 		}
 	}
-}*/
+}
 
-void BuildZP(double Z[nn][mm], double x[],int n,int m){
+/*void BuildZP(double Z[nn][mm], double x[],int n,int m){
 	for(int i=0; i<n; i++){
 		for(int j=0; j<m-1; j++){
 			Z[i][j] = pow(x[i],j);
 		}
 		Z[i][m-1] = sqrt(x[i]);
 	}
-}
+}*/
 
 void BuildZM(double Z[nn][mm], double x[nn][mm], int n, int m){
 	for (int i = 0; i < n; i++){
@@ -231,8 +231,7 @@ int main(){
 	//double y[nn] = {500, 700, 1000, 1200, 2200, 2650, 3750};
 	//double x[nn] = {2.5, 3.5, 4.5, 5.5, 6.5, 7.5};
 	//double y[nn] = {10, 9, 8, 6, 2, -2};
-	double xx[nn][mm] = { { 2.0, 1.0, 4.5 }, { 3.2, 1.2, 5.5 }, { 4.0, 1.5, 2.5 }, { 3.0, 1.8, 4.0 }, { 2.5, 2.0, 5.0 }
-};
+	double xx[nn][mm] = { { 2.0, 1.0, 4.5 }, { 3.2, 1.2, 5.5 }, { 4.0, 1.5, 2.5 }, { 3.0, 1.8, 4.0 }, { 2.5, 2.0, 5.0 }};
 	double y[nn] = { 30.1, 31.2, 24.0, 32.1, 35.1 };
 
 	double Z[nn][mm], a[mm], yf[nn];
@@ -240,13 +239,15 @@ int main(){
 	int m = mm - 1;
 
 	//BuildZP(Z, x, nn, mm);
+	
 	BuildZM(Z, xx, nn, mm);
 	printmatrix(xx, nn, mm);
-
+	cout << endl;
 	printmatrix(Z, nn, mm);
-
+	cout << endl;
 	NLRegress(Z, y, a, nn, mm);
-	
+	cout << endl;
+
 	multiply_matrix_to_vector(Z, a, yf, nn, mm);
 	for(int i = 0; i < nn; i++){
 		cout<<setw(10)<<xx[i]<<setw(10)<<y[i]<<setw(10)<<yf[i]<<endl;
